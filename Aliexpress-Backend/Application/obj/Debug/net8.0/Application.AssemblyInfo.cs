6ea780489c31e10709710988fe1d65ea00ff9833
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2025-03-27 09:46:30 +0200

    Переместил страницы в pages, добавил базовый код и стили на страницы.

--- Aliexpress-Backend/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Aliexpress-Backend/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fbcf8e48dc9ad8ea7925d29abb2febe836516d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74cd148adbb8584c1cfe40f8f76b72bee9f410b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aliexpress-Backend/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Aliexpress-Backend/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fbcf8e48dc9ad8ea7925d29abb2febe836516d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74cd148adbb8584c1cfe40f8f76b72bee9f410b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
