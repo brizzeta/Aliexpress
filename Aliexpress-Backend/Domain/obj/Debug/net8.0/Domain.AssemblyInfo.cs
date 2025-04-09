41684b2b8931bde17e3e02c219b358d1a57cb93c
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2025-04-09 10:09:57 +0300

    Создал иммитацию БД в db.json. Так же сделал базовую настройку иммитации бд.

--- Aliexpress-Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Aliexpress-Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a269d7fc8c6ccfbf0165ed68038ab17b898d5d5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f03dca4b1cfad1a56dcdc532e8c58df9cba5ab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
