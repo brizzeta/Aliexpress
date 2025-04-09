d11aab7bceda7d3940541d358b056ac95a8f0588
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2025-04-09 21:40:41 +0300

    Доделал десктоп баннера. Остался 1 баг. Чуть изменили анимации хедера.

--- Aliexpress-Frontend/src/pages/Home/home.component.ts
+++ Aliexpress-Frontend/src/pages/Home/home.component.ts
@@ -7,4 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent {}
