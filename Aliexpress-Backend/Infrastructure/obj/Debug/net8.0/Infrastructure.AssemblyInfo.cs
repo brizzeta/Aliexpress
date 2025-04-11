b8d2d837025c7ed0faecbfbf9f9280ac3aba8dbb
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2025-04-11 12:27:37 +0300

    Исправил подчеркивание у ссылки в баннере

--- Aliexpress-Backend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Aliexpress-Backend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d590e644e1cc3129770010e5932b5ce6e8db8651")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e7910881cb6c9914dc193021c3ad58fe2f9ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
