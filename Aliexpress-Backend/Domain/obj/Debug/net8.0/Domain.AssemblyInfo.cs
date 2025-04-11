b90326ed98e1c9e9a43c19be43ee6a952ec322a6
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2025-04-11 14:31:45 +0300

    Дофиксил адаптив у баннера

--- Aliexpress-Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Aliexpress-Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e7910881cb6c9914dc193021c3ad58fe2f9ff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d2d837025c7ed0faecbfbf9f9280ac3aba8dbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
