4eff4246fc6f5a1823b56f4912e334e30b335641
Original120005 <Original12006@gmail.com>
2025-04-14 21:42:00 +0300

    Тест ширины

--- Aliexpress-Frontend/src/pages/Home/Discounts/discounts.component.ts
+++ Aliexpress-Frontend/src/pages/Home/Discounts/discounts.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { ProductCardComponent } from '../product-card/product-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-discounts',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [CommonModule],
   templateUrl: './discounts.component.html',
   styleUrls: ['./discounts.component.scss']
 })
-export class DiscountsComponent { }
+export class DiscountsComponent {
+  discounts = [
+    { price: '15$', img: 'assets/images/Lamp.svg', alt: 'lamp' },
+    { price: '15$', img: 'assets/images/Iron.svg', alt: 'iron' },
+    { price: '15$', img: 'assets/images/Socks.svg', alt: 'socks' },
+    { price: '15$', img: 'assets/images/Pencils.svg', alt: 'pencils' },
+    { price: '15$', img: 'assets/images/Cup.svg', alt: 'cup' }
+  ];
+}
