cb2e8578bb0f79a79dd7210797add225c6bb4e02
DiktatorMira <112933359+DiktatorMira@users.noreply.github.com>
2025-05-25 13:23:55 +0300

    Фикс страницы и небольшие добавления

--- Aliexpress-Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Aliexpress-Backend/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a992d4ce97f2d4f22b6a2f75a3baa02acd69ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df248e2ccb8589768af3b0ad15e095208b6fbcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
